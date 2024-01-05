e8695020c5f50b4c6e350b9849cd5d5ec4b9699b
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-05 19:05:19 +0530

    inital setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      APP
+    </div>
+  );
+};
+
+export default React.memo(App);
