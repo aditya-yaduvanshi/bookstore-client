0c02617297bffafb9952cb789ea2b304cfa6e347
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-09 16:35:46 +0530

    feat: routing setup and auth routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "@/pages/signup";
+import Login from "@/pages/login";
+import Home from "@/pages/home";
 
 const App = () => {
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
-      APP
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="auth">
+          <Route path="signup" element={<Signup />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
