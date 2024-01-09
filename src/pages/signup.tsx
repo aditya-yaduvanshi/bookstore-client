25b67bb49bb9d2974621c0be64d8b3598067b536
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-09 16:35:22 +0530

    feat: signup page

---
+++ src/pages/signup.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+
+const Signup = () => {
+  return (
+    <>
+      <main className="flex flex-col justify-center items-center relative h-screen w-screen bg-[url('/cover.jpg')] bg-center bg-cover overflow-hidden">
+        <img
+          src="/logo.svg"
+          alt="pustakalay"
+          className="h-10 md:h-14 absolute top-16"
+        />
+        <form className="border shadow-lg p-5 rounded-xl w-full md:max-w-sm flex flex-col gap-5">
+          <section className="text-center">
+            <h2 className="text-lg text-semibold text-gray-800">SIGNUP</h2>
+            <p className="text-gray-600">Join pustakalay now!</p>
+          </section>
+          <section className="flex flex-col gap-2.5">
+            <Input name="name" type="text" placeholder="Full Name" />
+            <Input name="email" type="email" placeholder="Email" />
+            <Input name="password" type="password" placeholder="Password" />
+            <Input name="password2" type="password" placeholder="Confirm Password" />
+          </section>
+          <section className="flex flex-col justify-center items-center gap-2.5">
+            <Button variant="primary" color="primary" className="w-full">
+              Sign Up
+            </Button>
+            <p className="text-sm text-gray-600">
+              Don't have an account?{" "}
+              <Link
+                to="/auth/login"
+                className="text-blue-500 text-semibold hover:text-blue-700"
+              >
+                LogIn
+              </Link>
+            </p>
+          </section>
+        </form>
+      </main>
+    </>
+  );
+};
+
+export default React.memo(Signup);
