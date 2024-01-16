ae6087fdae5aa8a2de1df46430a84ee67e232e2a
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-16 18:40:31 +0530

    feat: books route and check if logged in

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "@/pages/signup";
 import Login from "@/pages/login";
 import Home from "@/pages/home";
+import Books from "@/pages/books";
+import PrivateOutlet from "@/hoc/PrivateOutlet";
+import useAuth from "@/hooks/useAuth";
 
 const App = () => {
+  const { checkLoginState } = useAuth();
+
+  useEffect(() => {
+    checkLoginState();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -12,6 +21,9 @@ const App = () => {
         <Route path="auth">
           <Route path="signup" element={<Signup />} />
           <Route path="login" element={<Login />} />
+        </Route>
+        <Route element={<PrivateOutlet />}>
+          <Route path="books" element={<Books />} />
         </Route>
       </Routes>
     </BrowserRouter>
