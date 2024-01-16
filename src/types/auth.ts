a109f1b0501fead448370b8a557a7dbf5c78af57
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-16 18:34:47 +0530

    feat: auth types

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,13 +1,25 @@
-export interface User {
+import { JwtPayload } from "jwt-decode";
+
+export type User = {
   name: string;
   email: string;
   _id: string;
-}
+};
 
-export interface AuthState {
-  user: User | null;
-  accessToken: string | null;
-  expireAt: number | null;
+export type AuthState = {
+  user?: User;
+  accessToken?: string;
+  expireAt?: number;
   loading: boolean;
-  error: string | null;
-}
+  error?: string;
+};
+
+export type LoginBody = Pick<User, "email"> & {
+  password: string;
+};
+
+export type SignupBody = LoginBody & Pick<User, "name">;
+
+export type JwtDecodedData = JwtPayload & {
+  user: AuthState["user"];
+};
