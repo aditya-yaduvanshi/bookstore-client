e9b3df29b53cc2791288f5c22ca2cfb1c6f5a31d
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-16 18:38:02 +0530

    feat: logic functionality

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,36 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useSearchParams, Navigate } from "react-router-dom";
+import SpinnerIcon from "~icons/svg-spinners/ring-resize";
 import Button from "@/components/Button";
 import Input from "@/components/Input";
+import useAuth from "@/hooks/useAuth";
 
 const Login = () => {
+  const {
+    auth: { loading, error, user },
+    setError,
+    login,
+  } = useAuth();
+  const [searchParams] = useSearchParams();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!email.trim()) return setError("Email is required!");
+    if (!password.trim()) return setError("Password is required!");
+
+    login({
+      email,
+      password,
+    });
+  };
+
+  if (user)
+    return (
+      <Navigate to={`/${searchParams.get("redirect") || "books"}`} replace />
+    );
+
   return (
     <>
       <main className="flex flex-col justify-center items-center relative h-screen w-screen bg-[url('/cover.jpg')] bg-center bg-cover overflow-hidden">
@@ -12,14 +39,39 @@ const Login = () => {
           alt="pustakalay"
           className="h-10 md:h-14 absolute top-16"
         />
-        <form className="sm:border sm:shadow-lg p-5 rounded-xl w-full sm:max-w-sm flex flex-col gap-5 sm:bg-white">
+        <form
+          onSubmit={handleSubmit}
+          className="sm:border sm:shadow-lg p-5 rounded-xl w-full sm:max-w-sm flex flex-col gap-5 sm:bg-white relative"
+        >
+          {loading && (
+            <div className="z-10 absolute top-0 bottom-0 left-0 right-0 flex justify-center items-center bg-white bg-opacity-75 rounded-xl">
+              <SpinnerIcon className="text-5xl text-blue-500" />
+            </div>
+          )}
           <section className="text-center">
             <h2 className="text-lg text-semibold text-gray-800">LOGIN</h2>
             <p className="text-gray-600">Login to your account</p>
           </section>
           <section className="flex flex-col gap-2.5 bg-white rounded-lg">
-            <Input name="email" type="email" placeholder="Email" />
-            <Input name="password" type="password" placeholder="Password" />
+            <Input
+              name="email"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {error ? (
+              <p className="border-red-500 text-red-500 bg-red-50 px-2.5 py-1.5 rounded-lg">
+                {error}
+              </p>
+            ) : null}
           </section>
           <section className="flex flex-col justify-center items-center gap-2.5">
             <Button variant="primary" color="primary" className="w-full">
