161239b28155ca93beae4d592ec0263cdd49ba4c
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-16 18:39:39 +0530

    feat: home action button link based on loggedin user

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Button from "@/components/Button";
+import useAuth from "@/hooks/useAuth";
 
 const Home = () => {
+  const {
+    auth: { user },
+  } = useAuth();
   return (
     <>
       <main className="flex justify-center items-center flex-col gap-2.5 h-screen w-screen bg-[url('/cover.jpg')] bg-center bg-cover overflow-hidden">
@@ -11,7 +15,11 @@ const Home = () => {
         </p>
         <img src="/logo.svg" alt="pustakalay" className="h-10 md:h-14" />
         <Button color="primary" variant="primary" className="px-5">
-          <Link to="/auth/login">JOIN NOW</Link>
+          {user ? (
+            <Link to="/books">BROWSE</Link>
+          ) : (
+            <Link to="/auth/login">JOIN NOW</Link>
+          )}
         </Button>
       </main>
     </>
